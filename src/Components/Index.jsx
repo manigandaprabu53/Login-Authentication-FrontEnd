3b8c94ac0648251c8850ea0b7736ea1acb2ff3bc
manigandaprabu53 <manigandaprabu53@gmail.com>
2024-09-05 12:56:35 +0530

    Added logout Button

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import useLogout from '../Hooks/useLogout'
 
 function Index() {
+  let logout = useLogout();
   return (
-    <div>index</div>
+    <div className='login-wrapper'>
+      <h3>Login SuccessFull</h3>
+      <Form>
+        <Form.Text>Click below button to logout</Form.Text>
+          <div className="text-align-center">
+            <Button variant="danger" onClick={()=>logout}>
+              Logout
+            </Button>
+        </div>
+      </Form>
+
+    </div>
   )
 }
 
